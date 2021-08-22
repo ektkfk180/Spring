b56e4b36ed73fb332b14d4ffa919d7fe56f406cb
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-08-22 20:50:32 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -11,7 +11,7 @@ public class MemberRepository {
 
     private static final MemberRepository instance = new MemberRepository();
 
-    static MemberRepository getInstance() {
+    public static MemberRepository getInstance() {
         return instance;
     }
 
